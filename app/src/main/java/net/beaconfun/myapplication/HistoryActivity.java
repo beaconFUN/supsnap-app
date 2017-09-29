a972ddbe2b4377f520c025c0c50aa0c6f07c013e
uehara <b1015062@fun.ac.jp>
2017-09-29 09:10:22 +0900

    add supsnap activity

--- app/src/main/java/net/beaconfun/myapplication/HistoryActivity.java
+++ app/src/main/java/net/beaconfun/myapplication/HistoryActivity.java
@@ -1,7 +1,9 @@
 package net.beaconfun.myapplication;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
 
 public class HistoryActivity extends AppCompatActivity {
 
@@ -10,4 +12,10 @@ public class HistoryActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_history);
     }
+
+    public void onTapped(View view) {
+        Intent intent = new Intent(this, SupSnapActivity.class);
+        startActivity(intent);
+    }
+
 }
