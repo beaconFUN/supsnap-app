8384a5e1726b6e5a0bbaf2f14469ef80ec42cf09
uehara <b1015062@fun.ac.jp>
2017-10-09 23:18:06 +0900

    add mock data

--- app/src/main/java/net/beaconfun/myapplication/HistoryActivity.java
+++ app/src/main/java/net/beaconfun/myapplication/HistoryActivity.java
@@ -16,15 +16,38 @@ import com.android.volley.toolbox.JsonObjectRequest;
 import org.json.JSONException;
 import org.json.JSONObject;
 
+import java.util.Date;
+
+import io.realm.Realm;
+import io.realm.RealmResults;
 
 public class HistoryActivity extends AppCompatActivity {
 
     private static final String TAG = "HistoryActivity";
+    private Realm myRealm;
+    private HistoryAdapter adapter;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_history);
+
+        myRealm = Realm.getDefaultInstance();
+
+        createMockData();
+    private void createMockData() {
+        myRealm.executeTransaction(new Realm.Transaction() {
+            @Override
+            public void execute(Realm realm) {
+                History history = realm.createObject(History.class, 1);
+                history.setLocation("未来大");
+                history.setCreatedAt(new Date());
+
+                History history2 = realm.createObject(History.class, 2);
+                history2.setLocation("函館山");
+                history2.setCreatedAt(new Date());
+            }
+        });
     }
 
     public void onTapped(View view) {
@@ -71,5 +94,11 @@ public class HistoryActivity extends AppCompatActivity {
     public void dialog(View view){
         DialogFragment dialog = new dialog();
         dialog.show(getFragmentManager(), "dialog_basic");
+    }
+
+    @Override
+    protected void onDestroy() {
+        super.onDestroy();
+        myRealm.close();
     }
 }
