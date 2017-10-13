b72f2a5935f5ebc24519123a263ed60ac8059170
uehara <b1015062@fun.ac.jp>
2017-10-13 17:30:28 +0900

    temporary commit

--- app/src/main/java/net/beaconfun/myapplication/HistoryActivity.java
+++ app/src/main/java/net/beaconfun/myapplication/HistoryActivity.java
@@ -1,9 +1,13 @@
 package net.beaconfun.myapplication;
 
 import android.content.Intent;
+import android.content.pm.PackageManager;
+import android.os.Build;
+import android.os.RemoteException;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.util.Log;
+import android.view.Menu;
 import android.view.View;
 import android.app.DialogFragment;
 import android.widget.ListView;
@@ -12,24 +16,41 @@ import com.android.volley.Request;
 import com.android.volley.Response;
 import com.android.volley.VolleyError;
 import com.android.volley.toolbox.JsonObjectRequest;
+
+import org.altbeacon.beacon.Beacon;
+import org.altbeacon.beacon.BeaconConsumer;
+import org.altbeacon.beacon.BeaconManager;
+import org.altbeacon.beacon.BeaconParser;
+import org.altbeacon.beacon.MonitorNotifier;
+import org.altbeacon.beacon.RangeNotifier;
+import org.altbeacon.beacon.Region;
 import org.json.JSONException;
 import org.json.JSONObject;
 
+import java.util.Collection;
 import java.util.Date;
 
 import io.realm.Realm;
 import io.realm.RealmResults;
 
-public class HistoryActivity extends AppCompatActivity {
-
+public class HistoryActivity extends AppCompatActivity implements BeaconConsumer {
+    private static final int PERMISSION_REQUEST_COARSE_LOCATION = 1;
     private static final String TAG = "HistoryActivity";
     private Realm myRealm;
     private HistoryAdapter adapter;
+    BeaconManager mBeaconManager;
+    Region region;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_history);
+
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.M) {
+            if (this.checkSelfPermission(android.Manifest.permission.ACCESS_COARSE_LOCATION) != PackageManager.PERMISSION_GRANTED) {
+                requestPermissions(new String[]{android.Manifest.permission.ACCESS_COARSE_LOCATION}, PERMISSION_REQUEST_COARSE_LOCATION);
+            }
+        }
 
         myRealm = Realm.getDefaultInstance();
 
@@ -40,6 +61,23 @@ public class HistoryActivity extends AppCompatActivity {
         adapter = new HistoryAdapter(histories);
         ListView listView = (ListView) findViewById(R.id.HistoryList);
         listView.setAdapter(adapter);
+
+
+        mBeaconManager = BeaconManager.getInstanceForApplication(this);
+        String IBEACON_FORMAT = "m:2-3=0215,i:4-19,i:20-21,i:22-23,p:24-24";
+        mBeaconManager.getBeaconParsers().add(new BeaconParser().setBeaconLayout(IBEACON_FORMAT));
+    }
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+        mBeaconManager.bind(this); // サービスの開始
+    }
+
+    @Override
+    protected void onPause() {
+        super.onPause();
+        mBeaconManager.unbind(this); // サービスの停止
     }
 
     private void createMockData() {
@@ -98,5 +136,52 @@ public class HistoryActivity extends AppCompatActivity {
     protected void onDestroy() {
         super.onDestroy();
         myRealm.close();
+    }
+
+    @Override
+    public void onBeaconServiceConnect() {
+        /*
+        mBeaconManager.addRangeNotifier(new RangeNotifier() {
+            @Override
+            public void didRangeBeaconsInRegion(Collection<Beacon> beacons, Region region) {
+                Log.i(TAG, "The first beacon I see is about "+beacons.toString()+" meters away.");
+                if (beacons.size() > 0) {
+
+                    Log.i(TAG, "The first beacon I see is about "+beacons.iterator().next().getDistance()+" meters away.");
+                }
+            }
+        });
+
+        try {
+            mBeaconManager.startRangingBeaconsInRegion(new Region("ACDD0D58-E9E2-4899-B183-86B765C61009", null, null, null));
+        } catch (RemoteException e) {
+            e.printStackTrace();
+        }
+        *
+        */
+        mBeaconManager.addMonitorNotifier(new MonitorNotifier() {
+            @Override
+            public void didEnterRegion(Region region) {
+                Intent intent = new Intent(getApplicationContext(), SupSnapActivity.class);
+                startActivity(intent);
+
+                Log.i(TAG, "I just saw an beacon for the first time!");
+            }
+
+            @Override
+            public void didExitRegion(Region region) {
+                Log.i(TAG, "I no longer see an beacon");
+            }
+
+            @Override
+            public void didDetermineStateForRegion(int state, Region region) {
+                Log.i(TAG, "I have just switched from seeing/not seeing beacons: "+state);
+            }
+        });
+
+        try {
+            region = new Region("acdd0d58-e9e2-4899-b183-86b765c61009", null, null, null);
+            mBeaconManager.startMonitoringBeaconsInRegion(region);
+        } catch (RemoteException e) {    }
     }
 }
